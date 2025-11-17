643f58a8708c1560c81ffc07a5587c945891f577
raidiniom <raidiniom22ph@gmail.com>
2025-11-17 20:28:09 +0800

    Started some work!

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,69 @@
-import { Text, View } from "react-native";
+import { Colors, GlobalStyles, UIStyles } from "@/styles/theme";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [msg, setMsg] = useState('');
+
+  const router = useRouter();
+
+  const handleMsg = (success: boolean) => {
+    if (success) {
+      setMsg('Login Successfully')
+    } else {
+      setMsg('Ivalid Credentials')
+    }
+  }
+
+  const handleLogin = () => {
+    const success = username === 'Admin' && password === 'Admin'
+    handleMsg(success)
+
+    if (success) {
+      router.push('/pages/homepage')
+    }
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={GlobalStyles.container}>
+      <Text style={GlobalStyles.titleText}>Welcome to GameWikia</Text>
+
+      <View style={UIStyles.container}>
+        <TextInput 
+          style={UIStyles.textInput}
+          placeholder="Username"
+          placeholderTextColor={Colors.mutedText}
+
+          value={username}
+          onChangeText={setUsername}
+        />
+        
+        <TextInput 
+          style={UIStyles.textInput}
+          placeholder="Password"
+          placeholderTextColor={Colors.mutedText}
+
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+
+        <TouchableOpacity style={UIStyles.loginButton} onPress={handleLogin}>
+          <Text style={UIStyles.loginButtonText}>Login</Text>
+        </TouchableOpacity>
+
+        {msg ? (
+          <Text style={{
+              color: msg === 'Login Successfully' ? 'green' : 'red',
+              marginTop: 10,
+              textAlign: 'center',}}>{msg}</Text>
+        ) : null}
+        
+      </View>
     </View>
+
   );
 }
---
+++ app/pages/homepage.tsx
@@ -0,0 +1,11 @@
+import { GlobalStyles } from "@/styles/theme";
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function homepage() {
+  return (
+    <View style={GlobalStyles.container}>
+      <Text>homepage</Text>
+    </View>
+  )
+}
